07c9e3976c539018a9901c365de95db5d69fd772
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-02-07 11:10:27 +0100

    add login

--- src/main/java/dk/easv/gui/controller/AppController.java
+++ src/main/java/dk/easv/gui/controller/AppController.java
@@ -1,30 +1,40 @@
 package dk.easv.gui.controller;
 
+import dk.easv.Main;
 import dk.easv.be.*;
 import dk.easv.gui.model.AppModel;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 
 public class AppController implements Initializable {
-    @FXML
-    private ListView<User> lvUsers;
-    @FXML
-    private ListView<Movie> lvTopForUser;
-    @FXML
-    private ListView<Movie> lvTopAvgNotSeen;
-    @FXML
-    private ListView<UserSimilarity> lvTopSimilarUsers;
-    @FXML
-    private ListView<TopMovie> lvTopFromSimilar;
 
-
+    public Pane loginPane;
+    @FXML
+    private BorderPane borderPane;
+    @FXML
+    private Group loginGroup;
     private AppModel model = new AppModel();
     private long timerStartMillis = 0;
     private String timerMsg = "";
+    private Stage stage;
+
 
     private void startTimer(String message){
         timerStartMillis = System.currentTimeMillis();
@@ -37,22 +47,15 @@ public class AppController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        lvUsers.setItems(model.getObsUsers());
-        lvTopForUser.setItems(model.getObsTopMovieSeen());
-        lvTopAvgNotSeen.setItems(model.getObsTopMovieNotSeen());
-        lvTopSimilarUsers.setItems(model.getObsSimilarUsers());
-        lvTopFromSimilar.setItems(model.getObsTopMoviesSimilarUsers());
 
-        startTimer("Load users");
-        model.loadUsers();
-        stopTimer();
-
-        lvUsers.getSelectionModel().selectedItemProperty().addListener(
-                (observableValue, oldUser, selectedUser) -> {
-                    startTimer("Loading all data for user: " + selectedUser);
-                    model.loadData(selectedUser);
-                });
     }
 
+    public void loginButton(ActionEvent actionEvent) throws IOException {
+        Parent newScene = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("views/AppTile.fxml")));
+        stage.setScene(new Scene(newScene));
+    }
 
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
 }
