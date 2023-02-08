eb326211626e19b94f641342f7aa67fdce0bbd17
Nicola Martinez <nicoenrique_1994@hotmail.com>
2023-02-08 12:05:26 +0100

    card added to class and controller

--- src/main/java/dk/easv/be/Card.java
+++ src/main/java/dk/easv/be/Card.java
@@ -1,4 +1,36 @@
 package dk.easv.be;
 
 public class Card {
+
+    private String title;
+
+    private String moviePhoto;
+
+    private String subTitleMovie;
+
+
+    public String getMoviePhoto() {
+        return moviePhoto;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSubTitleMovie() {
+        return subTitleMovie;
+    }
+
+    public void setMoviePhoto(String moviePhoto) {
+        this.moviePhoto = moviePhoto;
+    }
+
+    public void setSubTitleMovie(String subTitleMovie) {
+        this.subTitleMovie = subTitleMovie;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
 }
--- src/main/java/dk/easv/gui/controller/CardController.java
+++ src/main/java/dk/easv/gui/controller/CardController.java
@@ -1,4 +1,31 @@
 package dk.easv.gui.controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import dk.easv.be.Card;
 
 public class CardController {
-}
+        @FXML
+        private VBox Card;
+
+        @FXML
+        private ImageView MovieImage;
+
+        @FXML
+        private Label subTitleMovie;
+
+        @FXML
+        private Label titleMovie;
+
+        private setCards(Card cards){
+            //Image image = new Image(HERE THE IMAGE FROM API IN THE OBJECT);
+            //import image from the API
+            //MovieImage.setImage(image);
+            titleMovie.setText(cards.getTitle());
+            subTitleMovie.setText(cards.getSubTitleMovie());
+        }
+
+    }
+
