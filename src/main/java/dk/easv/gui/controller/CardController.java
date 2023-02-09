f6466d495a7690748e5593560e60a402b1402d3b
Patrik Valentiny <patrikvalentiny@gmail.com>
2023-02-09 21:09:29 +0100

    implement working image background

--- src/main/java/dk/easv/gui/controller/CardController.java
+++ src/main/java/dk/easv/gui/controller/CardController.java
@@ -1,36 +1,28 @@
 package dk.easv.gui.controller;
+
+import dk.easv.be.Card;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
-import dk.easv.be.Card;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CardController implements Initializable {
-        @FXML
-        private VBox vBox;
+    public GridPane gridPane;
 
-        @FXML
-        private ImageView movieImage;
-
-
-        @FXML
-        private Label titleMovie;
+    @FXML
+    private Label titleMovie;
 
 
-        public void setCards(Card card){
-            Image image = new Image(card.getMoviePhoto());
-            movieImage.setPreserveRatio(true);
-            movieImage.fitWidthProperty().bind(vBox.widthProperty());
-            movieImage.fitHeightProperty().bind(vBox.heightProperty());
-            //import image from the API
-            movieImage.setImage(image);
-            titleMovie.setText(card.getTitle());
-        }
+    public void setCards(Card card) {
+        gridPane.setStyle("-fx-background-image : url(" + card.getMoviePhoto() + ");" +
+                "-fx-background-size: cover;" +
+                "-fx-background-position: center center;" +
+                "-fx-background-repeat: none;");
+        titleMovie.setText(card.getTitle());
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
