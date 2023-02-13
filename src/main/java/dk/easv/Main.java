d3273c1bc8ed47e84fd2e4263495cbbea01b867d
jklaric <klaric9jakov@gmail.com>
2023-02-13 20:34:52 +0100

    Created night mode button and function

--- src/main/java/dk/easv/Main.java
+++ src/main/java/dk/easv/Main.java
@@ -11,19 +11,27 @@ import javafx.stage.Stage;
 import java.util.Objects;
 
 public class Main extends Application {
-
+    private static Parent root;
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(Main.class.getResource("views/Login.fxml"));
-        Parent root = loader.load();
+        root = loader.load();
         LoginController controller = loader.getController();
 
         primaryStage.setTitle("Movie Recommendation System 0.01 Beta");
         primaryStage.setScene(new Scene(root));
         primaryStage.setOnShown((event) -> controller.setStage(primaryStage));
         primaryStage.getIcons().add(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("icons/movie_projector.png"))));
+        root.getStylesheets().add(Main.class.getResource("/dk/easv/css/LoginStyle.css").toExternalForm());
         primaryStage.show();
         primaryStage.centerOnScreen();
+    }
+
+    public static void manageDisplayMode(){
+        if (!LoginController.checkIfDarkMode()){
+            root.getStylesheets().add(Main.class.getResource("/dk/easv/css/LoginStyle.css").toExternalForm());
+        }else
+            root.getStylesheets().add(Main.class.getResource("/dk/easv/css/LoginStyleDarkMode.css").toExternalForm());
     }
 
 
--- src/main/java/dk/easv/gui/controller/LoginController.java
+++ src/main/java/dk/easv/gui/controller/LoginController.java
@@ -12,9 +12,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
+import java.io.Console;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Objects;
@@ -28,11 +31,19 @@ public class LoginController implements Initializable {
     private MFXPasswordField passwordTextField;
     @FXML
     private GridPane loginGrid;
+    @FXML
+    private ImageView userIcon, usernameIcon, passwordIcon;
     private AppModel model = AppModel.getInstance();
     private long timerStartMillis = 0;
     private String timerMsg = "";
     private Stage stage;
-
+    private static Boolean isDarkMode = false;
+    private Image userImageDark = new Image(getClass().getResourceAsStream("/dk/easv/icons/userDark.png"));
+    private Image userNameImageDark = new Image(getClass().getResourceAsStream("/dk/easv/icons/userLogDark.png"));
+    private Image passwordImageDark = new Image(getClass().getResourceAsStream("/dk/easv/icons/lockIconLogDark.png"));
+    private Image userImage = new Image(getClass().getResourceAsStream("/dk/easv/icons/user.png"));
+    private Image userNameImage = new Image(getClass().getResourceAsStream("/dk/easv/icons/userLog.png"));
+    private Image passwordImage = new Image(getClass().getResourceAsStream("/dk/easv/icons/lockIconLog.png"));
 
     private void startTimer(String message){
         timerStartMillis = System.currentTimeMillis();
@@ -72,5 +83,26 @@ public class LoginController implements Initializable {
         this.stage = stage;
         stage.setMinWidth(loginGrid.getWidth());
         stage.setMinHeight(loginGrid.getHeight() + 30); // +30 because of the title bar and window border
+    }
+
+    public void switchDisplayMode(){
+        if(isDarkMode){
+            isDarkMode = false;
+            Main.manageDisplayMode();
+            userIcon.setImage(userImage);
+            usernameIcon.setImage(userNameImage);
+            passwordIcon.setImage(passwordImage);
+
+        }else {
+            isDarkMode = true;
+            Main.manageDisplayMode();
+            userIcon.setImage(userImageDark);
+            usernameIcon.setImage(userNameImageDark);
+            passwordIcon.setImage(passwordImageDark);
+        }
+    }
+
+    public static Boolean checkIfDarkMode(){
+        return isDarkMode;
     }
 }
